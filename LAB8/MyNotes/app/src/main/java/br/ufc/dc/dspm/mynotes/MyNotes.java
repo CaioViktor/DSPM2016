61687b386f902f1e93f864647700d6a0a712e11a
CaioViktor <caioviktor@hotmail.com>
2016-06-07 20:17:07 -0300

    Lab8 terminado

--- LAB8/MyNotes/app/src/main/java/br/ufc/dc/dspm/mynotes/MyNotes.java
+++ LAB8/MyNotes/app/src/main/java/br/ufc/dc/dspm/mynotes/MyNotes.java
@@ -5,14 +5,16 @@ import java.util.Iterator;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MyNotes extends Activity {
     private NoteDAO noteDAO;
-
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,14 +25,14 @@ public class MyNotes extends Activity {
         data+="/"+Calendar.getInstance().get(Calendar.YEAR);
         EditText dataEdit = (EditText)findViewById(R.id.date);
         dataEdit.setText(data);
+        listView = (ListView) findViewById(R.id.textViewNotes);
         listAll();
     }
 
     public void addNote(View view) {
         EditText titleText = (EditText) findViewById(R.id.editTextTitle);
-        EditText contentText = (EditText) findViewById(R.id.editTextContent);
         EditText dataEdit = (EditText)findViewById(R.id.date);
-
+        EditText contentText = (EditText)findViewById(R.id.editTextContent);
 
         Note note = new Note();
         note.setTitle(titleText.getText().toString());
@@ -43,37 +45,33 @@ public class MyNotes extends Activity {
     }
     private void listAll(){
         List<Note> notes = noteDAO.list();
-        if(notes != null){
+        updateList(notes);
 
-            Iterator<Note> it = notes.iterator();
-            StringBuffer buffer = new StringBuffer();
-            Note note;
-            while (it.hasNext()) {
-                note = it.next();
-                buffer.append(note.toString());
-                buffer.append("\n");
-            }
-            TextView list = (TextView) findViewById(R.id.textViewNotes);
-            list.setText(buffer.toString());
-        }
     }
     public void filtrar(View v){
         EditText dataINI = (EditText)findViewById(R.id.dataIni);
         EditText dataFIM = (EditText)findViewById(R.id.dataFim);
 
         List<Note> notes = noteDAO.listByIterval(dataINI.getText().toString(), dataFIM.getText().toString());
-        if(notes != null){
+        updateList(notes);
+    }
+    private void updateList(List<Note> notas){
+        if(notas != null && notas.size() > 0){
+            AdapterNote adapterNote = new AdapterNote(this,notas);
+            listView.setAdapter(adapterNote);
+            listView.setOnItemClickListener(adapterNote);
+            listView.setVisibility(View.VISIBLE);
+        }
+        else {
+            listView.setVisibility(View.INVISIBLE);
+        }
 
-            Iterator<Note> it = notes.iterator();
-            StringBuffer buffer = new StringBuffer();
-            Note note;
-            while (it.hasNext()) {
-                note = it.next();
-                buffer.append(note.toString());
-                buffer.append("\n");
+    }
+    protected void onActivityResult(int codReq,int codRes,Intent dados){
+        if(codReq == 0){
+            if(codRes == RESULT_OK){
+                listAll();
             }
-            TextView list = (TextView) findViewById(R.id.textViewNotes);
-            list.setText(buffer.toString());
         }
     }
 }
